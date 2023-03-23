c3b39b5175df9676172b23a27c9a7700981c3a4b
itskaramaman <singhkaramjeetaman@gmail.com>
2023-03-23 13:07:19 -0700

    worked on login and logout

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register
+from .views import register, profile
 from django.contrib.auth import views
 
 urlpatterns = [
     path("register/", register, name="users-register"),
-    path("login/", views.LoginView.as_view(), name="users-login"),
-    path("logout/", views.LogoutView.as_view(), name="users-logout")
+    path("login/", views.LoginView.as_view(template_name='users/login.html'), name="users-login"),
+    path("logout/", views.LogoutView.as_view(template_name='users/logout.html'), name="users-logout"),
+    path("profile/", profile, name="users-profile")
 ]
